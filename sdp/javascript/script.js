a5918754fd8183af8bf561288cdc892c13540134
Shreehari Acharya <shreehari.acharya.06@gmail.com>
2024-09-26 12:21:48 +0530

    All the code for SDP program

---
+++ sdp/javascript/script.js
@@ -0,0 +1,13 @@
+
+var a=100;
+var b=20;
+if(a>b){
+    document.getElementById("demo").innerHTML="a greater than b"
+}
+else if (a==b){
+    document.getElementById("demo").innerHTML= "a smalller then b"
+
+}
+else{
+    document.getElementById("demo").innerHTML="a bigger then b"
+}
